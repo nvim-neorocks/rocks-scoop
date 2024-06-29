45ed701f9980dc9731f48068ad04ab671d19b248
Marc Jakobi <marc.jakobi@tiko.energy>
2024-06-29 17:52:30 +0200

    chore: rename to rocks-scoop diff --git a/bin/auto-pr.ps1 b/bin/auto-pr.ps1 index 05bb484..e60b32b 100644 --- a/bin/auto-pr.ps1 +++ b/bin/auto-pr.ps1 @@ -1,6 +1,6 @@  param(      # overwrite upstream param -    [String]$upstream = "nvim-neorocks/scoop:main" +    [String]$upstream = "nvim-neorocks/rocks-scoop:main"  )
     if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "nvim-neorocks/scoop:main"
+    [String]$upstream = "nvim-neorocks/rocks-scoop:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
